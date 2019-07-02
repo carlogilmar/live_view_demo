979d8557412658b5b6629a8e5cf1d4127779abdf
Carlo Gilmar <carlogilmar12@gmail.com>
2019-07-01 23:24:16 -0500

    Step 3: Adding plugs in router

--- lib/counter_web/router.ex
+++ lib/counter_web/router.ex
@@ -5,8 +5,10 @@ defmodule CounterWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
+    plug Phoenix.LiveView.Flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :put_layout, {CounterWeb.LayoutView, :app}
   end
 
   pipeline :api do
