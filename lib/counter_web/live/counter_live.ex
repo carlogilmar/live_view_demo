5555759effbbdf781e5e01cea8953174976b8176
Carlo Gilmar <carlogilmar12@gmail.com>
2019-08-18 19:42:50 -0500

    Adding event for change div color

--- lib/counter_web/live/counter_live.ex
+++ lib/counter_web/live/counter_live.ex
@@ -8,7 +8,19 @@ defmodule CounterWeb.CounterLive do
   end
 
   def mount(_session, socket) do
-    {:ok, assign(socket, :val, 10)}
+    pixels = get_pixels()
+    socket =
+    socket
+      |> assign(:val, 10)
+      |> assign(:pixels, pixels)
+    {:ok, socket}
+  end
+
+  def get_pixels do
+    pixels = for index <- 1..1000, do: %{ id: "pixel_#{index}", class: "pixel" }
+    pixels
+    |> Enum.map(fn element -> {element.id, element} end)
+    |> Map.new()
   end
 
   def handle_event("inc", _, socket) do
@@ -17,6 +29,15 @@ defmodule CounterWeb.CounterLive do
 
   def handle_event("dec", _, socket) do
       {:noreply, update(socket, :val, &(&1 - 1))}
+  end
+
+  def handle_event("change", pixel_id, socket) do
+    pixels = get_pixels()
+    pixels= Map.put(pixels, pixel_id, %{class: "pixel_updated", id: pixel_id})
+    socket =
+      socket
+      |> assign(:pixels, pixels)
+    {:noreply, socket}
   end
 
 end
