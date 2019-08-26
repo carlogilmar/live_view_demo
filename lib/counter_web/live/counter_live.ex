3067c8b551304284e9835307d2de157be5729ecf
Carlo Gilmar <carlogilmar12@gmail.com>
2019-08-25 23:51:27 -0500

    Adding genserver calls in live view

--- lib/counter_web/live/counter_live.ex
+++ lib/counter_web/live/counter_live.ex
@@ -2,41 +2,35 @@ defmodule CounterWeb.CounterLive do
 
   use Phoenix.LiveView
   alias CounterWeb.CounterView
+	alias Counter.Pixels
+	alias CounterWeb.Endpoint
 
   def render(assigns) do
     CounterView.render("index.html", assigns)
   end
 
   def mount(_session, socket) do
-    pixels = get_pixels()
+		Endpoint.subscribe("grid")
+    pixels = Pixels.get_init_state()
     socket =
     socket
-      |> assign(:val, 10)
       |> assign(:pixels, pixels)
     {:ok, socket}
   end
 
-  def get_pixels do
-    pixels = for index <- 1..1000, do: %{ id: "pixel_#{index}", class: "pixel" }
-    pixels
-    |> Enum.map(fn element -> {element.id, element} end)
-    |> Map.new()
-  end
-
-  def handle_event("inc", _, socket) do
-      {:noreply, update(socket, :val, &(&1 + 1))}
-  end
-
-  def handle_event("dec", _, socket) do
-      {:noreply, update(socket, :val, &(&1 - 1))}
-  end
-
   def handle_event("change", pixel_id, socket) do
-    pixels = get_pixels()
+    pixels = Pixels.get_init_state()
     pixels= Map.put(pixels, pixel_id, %{class: "pixel_updated", id: pixel_id})
     socket =
       socket
       |> assign(:pixels, pixels)
+    {:noreply, socket}
+  end
+
+  def handle_info(%{event: "update_grid", payload: %{grid: grid}}, socket) do
+    socket =
+      socket
+      |> assign(:pixels, grid)
     {:noreply, socket}
   end
 
